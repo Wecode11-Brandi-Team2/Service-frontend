9014ced9018f1dc9962b1f6665788d32349b0b3c
조은별 <ebcebeb@gmail.com>
2020-09-22 11:25:01 +0900

    [조은별]Add: mypage 레이아웃 완료

--- src/pages/Mypage/Mypage.vue
+++ src/pages/Mypage/Mypage.vue
@@ -1,42 +1,137 @@
 <template>
-  <div class="myPageContainer">
-    <div class="TitleBox">
+  <div class="mypage-container">
+    <div class="title-box">
       <h1 class="title">마이페이지</h1>
     </div>
-    <div class="orderFrame">
-      <div class="iconBox">
-        <ul class="iconListWrapper">
-          <li class="iconList">
-            <a
-              class="on"
-              href="https://web-staging.brandi.co.kr/mypage/orderList"
-            >
+    <div class="order-frame">
+      <div class="icon-box">
+        <ul class="icon-listwrapper">
+          <li
+            v-for="(icon, idx) in icons"
+            v-bind:key="icon.icontext"
+            class="icon-list"
+            @click="selectItem(idx)"
+          >
+            <a class="on">
               <img
-                class="iconImg"
-                src="https://web-staging.brandi.co.kr/static/20.7.2/images/ic-mypage-orderlist-s@3x.png"/><br />
-              <span class="iconText">주문/배송조회</span>
-              <img
-                class="navArrow"
-                src="https://web-staging.brandi.co.kr/static/2020.7.2/images/ico_my_arrow_gray.png"
-            /></a>
+                class="icon-img"
+                :class="{ 'active-item': idx === activeItem }"
+                :mouseover="changeTextColor"
+                :src="icon.iconimage"/>
+              <br />
+              <div class="textarrow-wrapper">
+                <span
+                  class="icon-text"
+                  :class="{ 'active-font': idx === activeItem }"
+                  >{{ icon.icontext }}</span
+                >
+                <img
+                  class="nav-arrow"
+                  src="https://web-staging.brandi.co.kr/static/2020.7.2/images/ico_my_arrow_gray.png"
+                /></div
+            ></a>
           </li>
         </ul>
       </div>
     </div>
+    <OrderList
+      v-for="orderList in productInfo"
+      :key="orderList.orderNumber"
+      :productInfo="orderList"
+    />
   </div>
 </template>
 
 <script>
+import OrderList from './OrderList.vue';
+
 export default {
-  name: 'Mypage'
+  name: 'Mypage',
+  components: { OrderList },
+  data() {
+    return {
+      fontColor: 'black',
+      activeItem: 0,
+      productInfo: [
+        {
+          date: '2020.09.23',
+          orderNumber: 2020092300004000,
+          company: '밀리',
+          image:
+            'https://image.brandi.me/cproduct/2020/04/28/15899675_1588044782_image1_L.jpg',
+          productName: '윈드 아노락 세트',
+          option: '아이보리',
+          amount: 2,
+          price: 59040,
+          status: '배송중'
+        },
+        {
+          date: '2020.10.23',
+          orderNumber: 2020092300005500,
+          company: '블리즈',
+          image:
+            'https://image.brandi.me/cproduct/2020/04/28/15899675_1588053187_image3_L.jpg',
+          productName: '후드',
+          option: '블랙',
+          amount: 1,
+          price: 29000,
+          status: '배송완료'
+        }
+      ],
+
+      icons: [
+        {
+          id: 1,
+          iconlinkto: 'https://web-staging.brandi.co.kr/mypage/orderList',
+          iconimage:
+            'https://web-staging.brandi.co.kr/static/20.7.2/images/ic-mypage-orderlist-s@3x.png',
+          icontext: '주문/배송조회'
+        },
+        {
+          id: 2,
+          iconlinkto: 'https://web-staging.brandi.co.kr/mypage/point',
+          iconimage:
+            'https://web-staging.brandi.co.kr/static/2020.7.3/images/ic-my-point-s@3x.png',
+          icontext: '포인트'
+        },
+        {
+          id: 3,
+          iconlinkto: 'https://web-staging.brandi.co.kr/mypage/coupon',
+          iconimage:
+            'https://web-staging.brandi.co.kr/static/2020.7.3/images/ic-mypage-coupon-s@3x.png',
+          icontext: '쿠폰'
+        },
+        {
+          id: 4,
+          iconlinkto: 'https://web-staging.brandi.co.kr/mypage/qna',
+          iconimage:
+            'https://web-staging.brandi.co.kr/static/2020.7.3/images/ic-mypage-qna-s@3x.png',
+          icontext: 'Q & A'
+        },
+        {
+          id: 5,
+          iconlinkto: 'https://web-staging.brandi.co.kr/mypage/faq',
+          iconimage:
+            'https://web-staging.brandi.co.kr/static/2020.7.3/images/ic-myshopping-faq-s@3x.png',
+          icontext: 'FAQ'
+        }
+      ]
+    };
+  },
+  methods: {
+    selectItem(idx) {
+      this.activeItem = idx;
+    }
+  }
 };
 </script>
 
 <style scoped lang="scss">
-.myPageContainer {
-  .titleBox {
+.mypage-container {
+  .title-box {
     margin-top: 30px;
     padding: 40px 0px 20px 0px;
+
     .title {
       font-size: 30px;
       font-weight: normal;
@@ -44,43 +139,80 @@ export default {
       margin-bottom: 45px;
     }
   }
-  .orderFrame {
+  .order-frame {
     padding: 0px 20px;
-    margin: 0 auto;
-    .iconBox {
+    margin: 0 70px;
+
+    .icon-box {
       padding: 50px 0;
       border: 1px solid #000000;
       overflow: hidden;
       background-color: #fafafa;
       background: #fcfcfc;
-      .iconListWrapper {
-        .iconList {
+
+      .icon-listwrapper {
+        display: flex;
+
+        .icon-list {
           width: 20%;
           text-align: center;
           border-right: 1px solid #000;
+          &:last-child {
+            border-right: 0px;
+          }
           .on {
             font-weight: 700;
             color: #000000;
             font-size: 1.1em;
             text-decoration: none;
             cursor: pointer;
-            .iconImg {
+
+            .icon-img {
+              opacity: 0.4;
+              width: 75px;
+              margin-bottom: 8px;
+              cursor: pointer;
+            }
+
+            .active-item {
               opacity: 1;
               width: 75px;
+              margin-bottom: 8px;
               cursor: pointer;
             }
-            .iconText {
-              line-height: 18px;
-              vertical-align: top;
-              font-size: 1.1em;
-              font-weight: 700;
-              color: #000000;
-              cursor: pointer;
+
+            .textarrow-wrapper {
+              display: flex;
+              justify-content: center;
+
+              .icon-text {
+                margin-right: 4px;
+                line-height: 18px;
+                vertical-align: top;
+                font-size: 17px;
+                font-weight: normal;
+                color: #7d7d7d;
+                cursor: pointer;
+              }
+
+              .active-font {
+                margin-right: 4px;
+                line-height: 18px;
+                vertical-align: top;
+                font-size: 17px;
+                font-weight: bolder;
+                color: black;
+                cursor: pointer;
+              }
+
+              .nav-arrow {
+                width: 12px;
+                opacity: 1;
+              }
             }
-            .navArrow {
-              width: 12px;
-              opacity: 1;
-            }
+          }
+          a:hover .textarrow-wrapper .icon-text {
+            color: red;
           }
         }
       }
---
+++ src/pages/Mypage/OrderList.vue
@@ -0,0 +1,197 @@
+<template>
+  <div class="orderitem-wrapper">
+    <div class="orderitem-titlebox">
+      <h2>
+        {{ productInfo.date }}<span class="divider"></span
+        >{{ productInfo.orderNumber }}
+      </h2>
+      <div class="detail-btn">
+        <a class="btn-showdetail"
+          >주문상세보기<img
+            src="https://web-staging.brandi.co.kr/static/2020.7.3/images/ic-titleic-detailpage-moreaction@3x.png"
+            class="order-arrow"
+        /></a>
+      </div>
+    </div>
+    <div class="delivery-wrapper">
+      <div class="seller-title">판매자 배송 상품</div>
+      <div class="ordered-item">
+        <div class="shop-info">
+          <div class="shop-name">
+            <a>{{ productInfo.company }}</a>
+          </div>
+          <div class="empty"></div>
+          <div class="data">주문금액</div>
+          <div class="data">진행상황</div>
+        </div>
+        <div class="order-info">
+          <div class="image">
+            <a class="item-image"> <img :src="productInfo.image"/></a>
+          </div>
+          <div class="orderinfo-box">
+            <div class="item-name">
+              <a>{{ productInfo.productName }}</a>
+            </div>
+            <div class="order-data">{{ productInfo.option }}</div>
+            <div class="order-data">{{ productInfo.amount }} 개</div>
+          </div>
+          <div class="data1-wrapper">
+            <div class="data1">{{ productInfo.price }} 원</div>
+            <div class="data1">{{ productInfo.status }}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="btn-wrapper">
+      <button class="cancel-btn">주문취소</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['productInfo']
+};
+</script>
+
+<style scoped lang="scss">
+.orderitem-wrapper {
+  margin: 0 70px;
+  padding: 0 20px;
+
+  .orderitem-titlebox {
+    display: flex;
+    justify-content: space-between;
+    margin-top: 100px;
+    border-bottom: 3px solid #000;
+    font-weight: bold;
+    font-size: 26px;
+    padding-bottom: 16px;
+
+    .divider {
+      border-right: solid 2px #000000;
+      margin: 0 10px;
+      padding-top: 19px;
+      font-size: 0;
+      line-height: 0;
+    }
+    .btn-showdetail {
+      font-size: 24px;
+      margin-top: 3px;
+      text-decoration: none;
+      color: #000000;
+      font-weight: bold;
+      cursor: pointer;
+
+      .order-arrow {
+        margin-left: 4px;
+        margin-bottom: 7px;
+        height: 27px;
+        vertical-align: text-bottom;
+        width: 10px;
+        max-height: 20px;
+        cursor: pointer;
+      }
+    }
+  }
+  .delivery-wrapper {
+    border-bottom: 1px solid #000;
+
+    .seller-title {
+      font-weight: bold;
+      font-size: 26px;
+      padding-top: 20px;
+      padding-bottom: 20px;
+      border-bottom: 1px solid #000;
+    }
+    .ordered-item {
+      .shop-info {
+        display: flex;
+        align-items: center;
+        height: 64px;
+
+        .shop-name {
+          width: 126px;
+          padding: 19px 10px;
+          color: #000000;
+          cursor: pointer;
+          text-decoration: none;
+          font-size: 17px;
+          font-weight: bold;
+        }
+        .empty {
+          width: 673px;
+        }
+        .data {
+          font-weight: 400;
+          font-size: 16px;
+          letter-spacing: 0.7;
+          text-align: center;
+          width: 252px;
+          padding: 19px 10px;
+        }
+      }
+      .order-info {
+        display: flex;
+        align-items: center;
+        border-top: 1px solid #e1e1e1;
+        height: 126px;
+
+        .item-image {
+          padding: 15px 10px;
+          img {
+            width: 95px;
+            height: 95px;
+            margin-right: 10px;
+            cursor: pointer;
+          }
+        }
+        .orderinfo-box {
+          width: 673px;
+          padding: 10px;
+
+          .item-name {
+            font-weight: 400;
+            font-size: 1em;
+            margin-bottom: 4px;
+          }
+          .order-data {
+            color: #9e9e9e;
+            font-weight: 300;
+            font-size: 17px;
+            margin-top: 10px;
+          }
+        }
+        .data1-wrapper {
+          display: flex;
+          text-align: center;
+
+          .data1 {
+            font-size: 20px;
+            font-weight: bold;
+            width: 243px;
+          }
+        }
+      }
+    }
+  }
+  .btn-wrapper {
+    display: flex;
+    justify-content: center;
+
+    .cancel-btn {
+      margin-top: 30px;
+      background: #000000;
+      padding: 19px 0;
+      font-size: 20px;
+      width: 45%;
+      display: inline-block;
+      color: #ffffff;
+      cursor: pointer;
+      font-weight: lighter;
+      text-align: center;
+      letter-spacing: 1;
+    }
+  }
+}
+</style>
